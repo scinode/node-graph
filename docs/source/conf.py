010f4e95306cb8c2c102344ec5fd5852578af86d
superstar54 <xingwang1991@gmail.com>
2024-12-03 14:57:50 +0100

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,8 @@
 #
 import os
 import sys
+import shutil
+from pathlib import Path
 
 sys.path.insert(0, os.path.abspath("../.."))
 
@@ -62,3 +64,38 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_css_files = []
+
+
+# Function to copy HTML files
+def copy_html_files(app, exception):
+    """
+    Copy all .html files from source to build directory, maintaining the directory structure.
+    """
+    copy_print_info = "Copying HTML files to build directory"
+    print()
+    print(copy_print_info)
+    print(len(copy_print_info) * "=")
+    if exception is not None:  # Only copy files if the build succeeded
+        print(
+            "Build failed, but we still try to copy the HTML files to the build directory"
+        )
+    try:
+        src_path = Path(app.builder.srcdir)
+        build_path = Path(app.builder.outdir)
+
+        copy_print_info = f"Copying html files from sphinx src directory {src_path}"
+        print()
+        print(copy_print_info)
+        print(len(copy_print_info) * "-")
+        for html_file in src_path.rglob("*.html"):
+            relative_path = html_file.relative_to(src_path)
+            destination_file = build_path / relative_path
+            destination_file.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy(html_file, destination_file)
+            print(f"Copy {html_file} to {destination_file}")
+    except Exception as e:
+        print(f"Failed to copy HTML files: {e}")
+
+
+def setup(app):
+    app.connect("build-finished", copy_html_files)
