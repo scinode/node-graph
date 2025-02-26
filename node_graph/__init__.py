a812647fdf17440789a6b0e320a5bdd601fe93fd
superstar54 <xingwang1991@gmail.com>
2025-02-26 21:25:10 +0100

    Release 0.1.13

--- node_graph/__init__.py
+++ node_graph/__init__.py
@@ -1,8 +1,9 @@
 from .node_graph import NodeGraph
 from .node import Node
 from .decorator import node
+from .executor import NodeExecutor
 
-__version__ = "0.1.12"
+__version__ = "0.1.13"
 
 
-__all__ = ["NodeGraph", "Node", "node"]
+__all__ = ["NodeGraph", "Node", "node", "NodeExecutor"]
