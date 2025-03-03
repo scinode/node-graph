47e3f6340eb8f279d8a5ce6d81599d0cfa8fbf37
superstar54 <xingwang1991@gmail.com>
2025-03-03 12:06:39 +0100

    Add tests

---
+++ tests/test_pool.py
@@ -0,0 +1,12 @@
+from node_graph import NodePool, Node
+import pytest
+
+
+def test_node_pool():
+    assert "node_graph.test_add" in NodePool
+    assert isinstance(NodePool.node_graph.test_add.load()(), Node)
+    with pytest.raises(
+        AttributeError,
+        match="Namespace has no attribute 'test_add1'",
+    ):
+        NodePool.node_graph.test_add1
