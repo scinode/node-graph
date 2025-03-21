d11e5aec1e53b572e941743358423901aec73740
superstar54 <xingwang1991@gmail.com>
2025-03-21 07:05:05 +0100

    Release 0.1.27

--- src/node_graph/__init__.py
+++ src/node_graph/__init__.py
@@ -4,7 +4,7 @@ from .decorator import node
 from .executor import NodeExecutor
 from .nodes import NodePool
 
-__version__ = "0.1.26"
+__version__ = "0.1.27"
 
 
 __all__ = ["NodeGraph", "Node", "node", "NodeExecutor", "NodePool"]
