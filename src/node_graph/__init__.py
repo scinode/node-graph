98245341d48ae559cb07c7d66f866387f87983c7
superstar54 <xingwang1991@gmail.com>
2025-07-15 12:11:20 +0200

    Release 0.2.14

--- src/node_graph/__init__.py
+++ src/node_graph/__init__.py
@@ -5,7 +5,7 @@ from .executor import NodeExecutor
 from .nodes import NodePool
 from .group import group
 
-__version__ = "0.2.13"
+__version__ = "0.2.14"
 
 
 __all__ = ["NodeGraph", "Node", "node", "NodeExecutor", "NodePool", "group"]
