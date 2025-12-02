b9fdda783a82fe834ee4250479cea2f640f30a8b
Elliott Kasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-12-02 16:52:16 +0100

    Update src/node_graph/socket.py
    Co-authored-by: Xing Wang <xingwang1991@gmail.com>

--- src/node_graph/socket.py
+++ src/node_graph/socket.py
@@ -671,7 +671,7 @@ class TaskSocket(BaseSocket, OperatorSocketMixin):
         elif isinstance(value, TaggedValue) and value._socket is not None:
             self._task.graph.add_link(value._socket, self)
         elif self.property:
-            if self._parent._name == "inputs" and any(
+            if self._full_name.split(".")[0] == "inputs" and any(
                 [
                     link.to_socket._full_name_with_task == self._full_name_with_task
                     for link in self._links
